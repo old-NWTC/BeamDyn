5220f4476633ba46053dc08a1ec9766e20ec331d
Jonkman, Bonnie <Bonnie.Jonkman@nrel.gov>
2015-10-07 01:26:17 -0600

    updated version date. added files to create archive for release, including picture for BeamDyn_Manual. reorganized CertTest folder

---
+++ CertTest/CertTest.bat
@@ -0,0 +1,19 @@
+@echo off
+@SET Compare=FC /T
+@SET BeamDyn=..\bin\BeamDyn_Driver.exe
+@set CompareFile=CertTest.out
+@SET Editor=NotePad.EXE
+
+
+@IF EXIST %CompareFile%  DEL %CompareFile%
+
+%BeamDyn% Dvr_5MW_Static.inp
+%Compare% Dvr_5MW_Static.out                     Results\Dvr_5MW_Static.out               >> %CompareFile%
+%Compare% Static_BeamDyn_Input_5MW.inp.sum       Results\Static_BeamDyn_Input_5MW.inp.sum >> %CompareFile%
+
+
+%BeamDyn% Dvr_5MW_Dynamic.inp
+%Compare% Dvr_5MW_Dynamic.out                   Results\Dvr_5MW_Dynamic.out               >> %CompareFile%
+%Compare% Dynamic_BeamDyn_Input_5MW.inp.sum     Results\Dynamic_BeamDyn_Input_5MW.inp.sum >> %CompareFile%
+
+%Editor% %CompareFile%
